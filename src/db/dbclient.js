088c3691493a6901d94c626e4aaa25b1180ab7be
Raaja SN <raaja.017@gmail.com>
2023-09-23 13:41:30 +0530

    Installed pm2

--- src/db/dbclient.js
+++ src/db/dbclient.js
@@ -4,7 +4,7 @@ const sellerdb = require('../seller/sellerdb/sellerdb')
 const productdb = require('../product/productdb/productdb')
 const orderdb = require('../order/orderdb/orderdb')
 
-const DB_URL = process.env.DB_URL || 'mongodb+srv://dellseradmin:Qefdb7Z3tg45ow9N@dsellercluster.cwdi2eo.mongodb.net/?'
+const DB_URL = process.env.DB_URL 
 
 const connectDB = async ()=>{
     await moongose.connect(DB_URL,{autoIndex:false,useNewUrlParser:true})
